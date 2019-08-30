6b3c744628926f48e19d3c0730dfb3fd38d9ff20
Mao39 <steallermail@gmail.com>
2019-08-30 12:10:53 +0200

    fix style of code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 
 import readlineSync from 'readline-sync';
 
-export const askName = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}`);
-}
+const askName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
+};
+
+export default askName;
