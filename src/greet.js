8d508a0c561d3a5bbdc834eac5895fc9721d1231
Mao39 <steallermail@gmail.com>
2019-08-30 20:10:13 +0200

    add greeting for brain-progression

--- src/greet.js
+++ src/greet.js
@@ -5,6 +5,7 @@ const greet = (gameName) => {
   if (gameName === 'brain-even') console.log('Answer "yes" if number even otherwise answer "no".');
   if (gameName === 'brain-calc') console.log('What is the result of the expression?');
   if (gameName === 'brain-gcd') console.log('Find the greatest common divisor of given numbers.');
+  if (gameName === 'brain-progression') console.log('What number is missing in the progression?');
 
   const name = readlineSync.question('\nMay I have your name? ');
   console.log(`Hello, ${name}`);
