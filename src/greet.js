f84ee0f05eef881790fb4997f44c60ff20b852b2
Mao39 <steallermail@gmail.com>
2019-08-30 15:12:22 +0200

    change greeting for brain-even

--- src/greet.js
+++ src/greet.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 
-const greet = () => {
-  console.log('Welcome to the Brain Games!\n');
-  const name = readlineSync.question('May I have your name? ');
+const greet = (gameName) => {
+  console.log('Welcome to the Brain Games!');
+  if (gameName === 'brain-even') console.log('Answer "yes" if number even otherwise answer "no".');
+
+  const name = readlineSync.question('\nMay I have your name? ');
   console.log(`Hello, ${name}`);
 };
 
