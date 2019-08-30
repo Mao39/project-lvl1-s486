10debd28c3a4ff2dbf78a8a37e0fe3aa33dcdcdc
Mao39 <steallermail@gmail.com>
2019-08-30 21:53:04 +0200

    add greeting for brain-prime

--- src/greet.js
+++ src/greet.js
@@ -8,6 +8,7 @@ const greet = (gameName) => {
   if (gameName === 'brain-calc') console.log('What is the result of the expression?');
   if (gameName === 'brain-gcd') console.log('Find the greatest common divisor of given numbers.');
   if (gameName === 'brain-progression') console.log('What number is missing in the progression?');
+  if (gameName === 'brain-prime') console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
   const name = readlineSync.question('\nMay I have your name? ');
   console.log(`Hello, ${name}`);
