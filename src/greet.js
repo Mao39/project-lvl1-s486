0f6c4a6ffbea1b690110e48021bb936bee783310
Mao39 <steallermail@gmail.com>
2019-08-30 21:37:50 +0200

    delete question from brain-games

--- src/greet.js
+++ src/greet.js
@@ -2,6 +2,8 @@ import readlineSync from 'readline-sync';
 
 const greet = (gameName) => {
   console.log('Welcome to the Brain Games!');
+
+  if (gameName === 'brain-games') return null;
   if (gameName === 'brain-even') console.log('Answer "yes" if number even otherwise answer "no".');
   if (gameName === 'brain-calc') console.log('What is the result of the expression?');
   if (gameName === 'brain-gcd') console.log('Find the greatest common divisor of given numbers.');
